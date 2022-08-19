3fddbf03d4c6f446b87180fb6aea5617829c792f
alexandrhazov <khazey3@gmail.com>
2022-08-19 16:23:39 +0300

    Commit 3

---
+++ src/test/java/Helpers/TestValues.java
@@ -0,0 +1,8 @@
+package Helpers;
+
+public class TestValues {
+
+    public static String TEST_BODY = "Message to getBlend tech support";
+    public static String TEST_TITLE = "Alex";
+    public static String TEST_EMAIL = "Test@mail.ru";
+}
